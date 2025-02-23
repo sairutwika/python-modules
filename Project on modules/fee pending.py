eda93e7c0f735dfb48bb222408477b5f0790bcbc
sairutwika <113226326+sairutwika@users.noreply.github.com>
2025-02-23 11:47:11 +0530

    Add files via upload

---
+++ Project on modules/fee pending.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_mails(complete_data):
+    for data in complete_data:
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 587
+
+        username = "sairutwika1408@gmail.com"
+        password = "lkte omxr usrc jmcn"
+
+        from_email = "sairutwika1408@gmail.com"
+        to_email = data[1]
+        subject = "Fee Update"
+        body = f"Dear {data[0]},\nYou have Fee Pending\nPlease make sure to clear it as soon as possible !"
+
+        msg = MIMEMultipart()
+        msg['From'] = from_email    
+        msg['To'] = to_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body,'plain'))
+
+        server = smtplib.SMTP(smtp_server,smtp_port)
+        server.starttls()
+        server.login(username,password)
+        server.send_message(msg)
+        server.quit()
+        print(f"Mail sent to {data[0]}")
+
+
---
+++ Project on modules/otp.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import random
+
+def send_otp(data):
+    
+    otp = random.randint(1111,9999)
+
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+
+    username = "sairutwika1408@gmail.com"
+    password = "lkte omxr usrc jmcn    "
+
+    from_email = "sairutwika1408@gmail.com"
+    to_email = data
+    subject = "OTP Validation"
+    body = f"OTP for Validation is {otp}. \nThank You."
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body,'plain'))
+
+    server = smtplib.SMTP(smtp_server,smtp_port)
+    server.starttls()
+    server.login(username,password)
+    server.send_message(msg)
+    server.quit()
+
+    return otp
+
